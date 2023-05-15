ba76b1a0e6c01f00ced7d8efdd342c4cc822eae5
AlbertoDPH <114424982+AlbertoDPH@users.noreply.github.com>
2023-05-15 09:24:19 +0200

    Modificacion list-cliente.components

--- altaCliente/src/app/list-cliente/list-cliente.component.ts
+++ altaCliente/src/app/list-cliente/list-cliente.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-cliente',
@@ -7,13 +7,22 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ListClienteComponent implements OnInit{
 
-  @Input() clientes: any[];
 
+  nombre ='';
+  cif='';
+  direccion='';
+  grupo='';
 
   constructor() { }
 
   ngOnInit(): void {
 
+  }
+
+  guardar() {
+    console.log('Nombre:',this.nombre);
+    console.log('CIF:',this.cif);
+    console.log('Direccion:',this.direccion);
   }
 
 }
